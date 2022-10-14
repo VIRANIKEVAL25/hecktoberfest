b3e55c5efa7f2c2c72f94708f34b96e7eee975f5
kevalvirani <78257489+kevalvirani@users.noreply.github.com>
2022-10-14 14:22:06 +0530

    updated file
    Hello, please check i changed your code.

--- contents.dart
+++ contents.dart
@@ -1,30 +1,138 @@
-class OnboardingContent {
-  String image;
-  String title;
-  String description;
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:thesecurityman/constants.dart';
+import 'package:thesecurityman/login.dart';
+import 'package:thesecurityman/registerDashboard.dart';
 
-  OnboardingContent({this.image, this.title, this.description});
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
 }
 
-List<OnboardingContent> contents = [
-  OnboardingContent(
-      image: 'assets/online_platform.json',
-      title: 'The Online Platform',
-      description:
-                 "The Security Man is a Startup Idea that provides an Online Security"
-                 " Service to Organizations and Companies for their Properties. "
-                 "We Work with the Security Man and the Security Needed Organization on an Online Platform."
-  ),
-  OnboardingContent(
-      image: 'assets/hour-lottie.json',
-      title: '24*7 Support',
-      description: "Customers Satisfaction will be the priority for Us. "
-                   "We provide 24*7 customer support for security complaints. "
-                   "We will try to resolve the problem with an efficient solution on time."),
-  OnboardingContent(
-      image: 'assets/Job-lottie.json',
-      title: 'The Job Opportunity',
-      description: "Our Start-Up Idea helps Security Man to look for a job. "
-                   "And the Online Platform for Security Companies to grow their business with us. "
-                   "Online Platform helps their businesses in expanding and growing in the competitive world."),
-];
+class HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    var images = ["assets/Customer.jpg","assets/Splash.jpg","assets/business-partner.jpg"];
+   
+    return Scaffold(
+     /* appBar: AppBar(
+        title: Text("The Security Man"),
+        backgroundColor: Color(0xFF23408e), //Color(0xFF23408e)
+        toolbarHeight: 70,
+        //automaticallyImplyLeading: false,
+      ), */
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 100,
+              ),
+              Text(
+                "The Security Man",
+                style: TextStyle(fontSize: 45,fontWeight: FontWeight.bold,color: mainColor,fontFamily: 'Hina'),),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Login As",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontFamily: 'Hina',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              RaisedButton(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 120),
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => Login(value: images[0],identity: 'Customer',)));
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                color: mainColor,
+                child: Text(
+                  "Customer",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Hina'),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              RaisedButton(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 110),
+                onPressed: () {
+                  navigator.push
+                      context,
+                      MaterialPageRoute(builder: (context) => Login(value: images[1],identity: 'Security Man',)));
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                color: mainColor,// Color(0xFF195ba5),
+                child: Text(
+                  "Security Man",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Hina'),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              RaisedButton(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 90),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login(value: images[2],identity: 'Business Partner',)));
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                color: mainColor,//Color(0xFF23408e),
+                child: Text(
+                  "Business Partner",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Hina'),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextButton(
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterDashboard()));
+                  },
+                  child: Text(
+                    "Not Registered!",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,fontFamily: 'Hina',fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline),)
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
